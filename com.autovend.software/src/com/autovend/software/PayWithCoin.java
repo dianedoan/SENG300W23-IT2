f3362e8759fa1e03902b3691c9d005709cf01f83
delaram <delaram.bahreiniesfa@ucalgary.ca>
2023-03-29 00:00:03 -0600

    changeHandle method added

--- com.autovend.software/src/com/autovend/software/PayWithCoin.java
+++ com.autovend.software/src/com/autovend/software/PayWithCoin.java
@@ -42,7 +42,8 @@ public class PayWithCoin implements CoinSlotObserver, CoinValidatorObserver, Coi
 			private CustomerIO customer;
 			private AttendantIO attendant;
 			private CashIO cash;
-			ReceiptPrinter printer;
+			private ReceiptPrinter printer;
+			private string message;
 			
 			// Array of the number of each coin denomination to dispense for change
 			private int[] coinsChange = new int[5];
@@ -71,6 +72,7 @@ public class PayWithCoin implements CoinSlotObserver, CoinValidatorObserver, Coi
 //			public BigDecimal getTotal(BigDecimal total) {
 //				return this.totalCoin;
 //			}
+			
 
 			public PayWithCoin(SelfCheckoutStation selfCheckoutStation, CustomerIO customer) throws DisabledException, OverloadException{
 				
@@ -82,6 +84,26 @@ public class PayWithCoin implements CoinSlotObserver, CoinValidatorObserver, Coi
 					attendant = new AttendantIO();
 					cash = new CashIO();
 					//selfCheckoutStation = sc;
+					selfCheckoutStation.coinValidator.register(this);
+					selfCheckoutStation.coinInput.register(this);
+					selfCheckoutStation.coinDispensers.get(5).register(this);
+					selfCheckoutStation.coinDispensers.get(10).register(this);
+					selfCheckoutStation.coinDispensers.get(25).register(this);
+					selfCheckoutStation.coinDispensers.get(10).register(this);
+					selfCheckoutStation.coinDispensers.get(200).register(this);
+					
+					// Initialize the five dispensers for each denomination being used in the machine
+					dispenser5 = selfCheckoutStation.billDispensers.get(5);
+					dispenser10 = selfCheckoutStation.billDispensers.get(10);
+					dispenser20 = selfCheckoutStation.billDispensers.get(25);
+					dispenser50 = selfCheckoutStation.billDispensers.get(100);
+					dispenser100 = selfCheckoutStation.billDispensers.get(200);
+					
+					dispensers[0] = dispenser5;
+					dispensers[1] = dispenser10;
+					dispensers[2] = dispenser50;
+					dispensers[3] = dispenser100;
+					dispensers[4] = dispenser200;
 				
 			}
 			
@@ -105,8 +127,124 @@ public class PayWithCoin implements CoinSlotObserver, CoinValidatorObserver, Coi
 					
 				}
 			}
+			
+			public boolean changeHandle() throws DisabledException, EmptyException, OverloadException {
+				
+				// check if the remaining amount is greater than zero
+				if(remainingAmount > 0) {
+					// Return false as bill not fully paid and thus no change given
+					return false;
+				}
+				
+				// If the remaining amount is less than 0 the change has to be returned to the customer
+				else if (remainingAmount < 0) {
+					// signal to CASHIO the amount of change due
+					cash.getChange(remainingAmount);
+					this.cash.setChange(-remainingAmount);
+//					this.cash.setChange(Double.toString(-remainingAmount));
+					this.customer.setAmount(0);
+					
+					if (checkChange()) {
+					//dispense the change to the customer
+					coinDispenser emitter;
+					for (int i = 0; i < coinsChange.length; i++) {
+						for (int j = 0; j < coinsChange[i]; j++) {
+							emitter = dispensers[i];
+							emitter.emit();
+			
+						}
+					}
+					// Return true as there was sufficient change and it was dispensed
+					return true;
+					}
+					else {
+						//if insufficient change signal attendant and suspendSystem
+						//attendant.informAttendant();
+						
+						suspendSystem();
+						// Return false as there was not sufficient change to dispense
+						return false;
+					}
+				}
+				else {
+					
+					// see PrintReceipt
+					// Return false if no change given
+					return false;
+				}	
+			}
 	
-	
+			/**
+			 * When the attendant is called the system has to be blocked from any further transactions
+			 */
+			public void suspendSystem() {
+				selfCheckoutStation.mainScanner.disable();
+				selfCheckoutStation.handheldScanner.disable();
+				selfCheckoutStation.scale.disable();
+				selfCheckoutStation.printer.disable();
+				selfCheckoutStation.billInput.disable();
+				selfCheckoutStation.billOutput.disable();
+				selfCheckoutStation.billStorage.disable();
+				selfCheckoutStation.billValidator.disable();
+			}
+			
+			public boolean checkChange() {
+				
+				// Get the amount of change, converted to integer as coins not yet supported
+				int changeDue = (int) this.cash.getChange();
+				
+				// Get the number of coins in each dispenser
+				int size5 = dispenser5.size();
+				int size10 = dispenser10.size();
+				int size25 = dispenser25.size();
+				int size100 = dispenser100.size();
+				int size200 = dispenser200.size();
+				
+				// Check if all are empty, and if so notify listeners
+				if ((size5 + size10 + size20 + size50 + size100) == 0) {
+					this.reactToBillsEmptyEvent(dispenser5);
+					this.reactToBillsEmptyEvent(dispenser10);
+					this.reactToBillsEmptyEvent(dispenser20);
+					this.reactToBillsEmptyEvent(dispenser50);
+					this.reactToBillsEmptyEvent(dispenser100);
+				}
+				
+				// Calculate number of each coin denomination in decreasing order that can be given
+				int change200 = Math.min((changeDue / 200),size200);
+				changeDue -= (change200 * 200);
+				int change100 = Math.min((changeDue / 100),size100);
+				changeDue -= (change100 * 100);
+				int change25 = Math.min((changeDue / 25),size25);
+				changeDue -= (change25 * 25);
+				int change10 = Math.min((changeDue / 10),size10);
+				changeDue -= (change10 * 10);
+				int change5 = Math.min((changeDue / 5),size5);
+				changeDue -= (change5 * 5);
+				
+				// Check if change due at the end equals zero
+				if (changeDue == 0) {
+					// If so, update the array for number of each bill to dispense and return true
+					billsChange[0] = change5;
+					billsChange[1] = change10;
+					billsChange[2] = change25;
+					billsChange[3] = change100;
+					billsChange[4] = change200;
+					return true;
+				}
+				// If not, there are not enough bills or change cannot be made with just bills, return false
+				else {
+					return false;
+				}
+				
+			}
+			
+			/**
+			 * Getter to retrieve information for testing on which bills dispensed as change
+			 * @return billsChange array of number of each bill given as change
+			 */
+			public int[]  getCoinsChange() {
+				return coinsChange;
+			}
 			
 			private boolean coinsLoadedEvent = false;
 			private boolean coinInsertedEvent = false;
