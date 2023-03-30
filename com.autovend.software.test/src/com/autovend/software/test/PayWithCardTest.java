3b3e780aee79d278731e016b5be517a9bd672b33
dianedoan <Dianedd419@hotmail.com>
2023-03-30 12:47:14 -0600

    updated test

--- com.autovend.software.test/src/com/autovend/software/test/PayWithCardTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/PayWithCardTest.java
@@ -2,6 +2,8 @@ package com.autovend.software.test;
 
 import static org.junit.Assert.*;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Currency;
 
@@ -11,13 +13,18 @@ import org.junit.Test;
 
 import com.autovend.*;
 import com.autovend.devices.*;
-import com.autovend.external.ProductDatabases;
-import com.autovend.products.*;
 import com.autovend.software.*;
 
 public class PayWithCardTest {
 	private SelfCheckoutStation station;
 	private SelfCheckoutMachineLogic machineLogic;
+
+	static Card credit_card;
+	static Card debit_card;
+	
+	static BufferedImage signature;
+	
+	static String pin;
 	
 	/**
 	 * Set up before each test.
@@ -29,6 +36,14 @@ public class PayWithCardTest {
 		BigDecimal[] coinDenominations = {BigDecimal.valueOf(0.05), BigDecimal.valueOf(0.10), BigDecimal.valueOf(0.25)};
 		station = new SelfCheckoutStation(currency, billDenominations, coinDenominations, 10000, 5);
 		machineLogic = new SelfCheckoutMachineLogic(station);
+		
+		pin = "0000";
+		credit_card = new CreditCard("Mastercard", "1234567", "Adam", "123", pin, true, true);
+		debit_card = new DebitCard("VISA", "1234567", "Bob", "123", pin, true, true);
+		
+		signature = new BufferedImage(24, 24, 13);
+		
+		station.cardReader.enable();
 	}
 
 	/**
@@ -36,17 +51,71 @@ public class PayWithCardTest {
 	 */
 	@After
 	public void tearDown() {
+		station.cardReader.disable();
+		
 		station = null;
-		machineLogic = null;	
+		machineLogic = null;
+		
+		credit_card = null;
+		debit_card = null;
 	}
-
 	
 	/**
-	 * Tests when ...
+	 * Tests when a credit card is tapped more than four times.
 	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void reachMaximumAttemptTap() throws IOException {
+		station.cardReader.tap(credit_card);
+		machineLogic.payWithCard();
+		station.cardReader.tap(credit_card);
+		machineLogic.payWithCard();
+		station.cardReader.tap(credit_card);
+		machineLogic.payWithCard();
+		station.cardReader.tap(credit_card);
+		machineLogic.payWithCard();
+	}
+	
+	/**
+	 * Tests when a credit card is swiped more than four times.
+	 */
+	@Test
+	public void reachMaximumAttemptSwipe() throws IOException {
+		station.cardReader.swipe(credit_card, signature);
+		machineLogic.payWithCard();
+		station.cardReader.swipe(credit_card, signature);
+		machineLogic.payWithCard();
+		station.cardReader.swipe(credit_card, signature);
+		machineLogic.payWithCard();
+		station.cardReader.swipe(credit_card, signature);
+		machineLogic.payWithCard();
 	}
 
+	/**
+	 * Tests when a credit card is inserted and removed more than four times. 
+	 */
+	@Test (expected = SimulationException.class)
+	public void reachMaximumAttemptInsert() throws IOException {
+		station.cardReader.insert(credit_card, pin);
+		machineLogic.payWithCard();
+		station.cardReader.remove();
+		station.cardReader.insert(credit_card, pin);
+		machineLogic.payWithCard();
+		station.cardReader.remove();
+		station.cardReader.insert(credit_card, pin);
+		machineLogic.payWithCard();
+		station.cardReader.remove();
+		station.cardReader.insert(credit_card, pin);
+		machineLogic.payWithCard();
+		station.cardReader.remove();
+	}
+	
+	/**
+	 * Tests when a credit card is tapped once.
+	 */
+	@Test
+	public void cardTap() throws IOException {
+		station.cardReader.tap(credit_card);
+		machineLogic.payWithCard();
+
+	}
 }
